795946f276fb2ef7a97eae1472779d53d71d313e
Necip <ncptzgn@gmail.com>
2021-03-05 15:31:55 +0300

    Asenkron Yapılar

---
+++ fetch-promiseHTTP.js
@@ -0,0 +1,22 @@
+class Request {
+    get(url){ // Get Request
+        return new Promise((resolve,reject)=>{
+        
+        fetch(url)
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+        })
+    }
+}
+
+const request = new Request();
+let albums;
+
+request.get("https://jsonplaceholder.typicode.com/albums")
+.then(albums => {
+    console.log(albums);
+})
+.catch(err => console.log(err));
+
+// console.log(albums); // Asenkron
